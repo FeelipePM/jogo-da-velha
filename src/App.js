eac8d60c192cd0e64f9624fbcf0bf07525eaeb74
Felipe Machado <feelipe@email.com>
2019-08-07 17:36:03 -0300

    [049] - Basic challenge

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
+import Logo from "./objects/Logo/index.js";
 import About from "./objects/About/index.js";
-import logo from "./img/collabcode.svg";
-import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <About>Felipe</About>
+        <Logo />
+        <About />
       </header>
     </div>
   );
