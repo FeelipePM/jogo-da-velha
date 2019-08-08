d68ef166479b02bd6daa5b39761cba560fee5909
Felipe Machado <feelipe@email.com>
2019-08-08 11:27:05 -0300

    [049] - Advanced challenge

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from "react";
-import Logo from "./objects/Logo/index.js";
-import About from "./objects/About/index.js";
-import Menu from "./objects/Menu/index.js";
+
+import Header from "./components/Header/index.js";
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Logo />
-        <About />
-        <Menu />
-      </header>
-    </div>
-  );
+  return <Header />;
 };
 
 export default App;
