311a4d11180d7ce0a9cd2e384fcd4a269ffc39da
Felipe Machado <feelipe@email.com>
2019-08-12 11:47:20 -0300

    [050] - intermediate challenge

--- src/objects/WhiteBoard/index.js
+++ src/objects/WhiteBoard/index.js
@@ -1,9 +1,17 @@
 import React from "react";
 
+import Circle from "../Circle/index.js";
+import X from "../X/index.js";
+
 import "./styles.css";
 
 const Whiteboard = () => {
-  return <div className="white-board" />;
+  return (
+    <div className="white-board">
+      <Circle />
+      <X />
+    </div>
+  );
 };
 
 export default Whiteboard;
---
+++ src/objects/X/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import x from "../../img/x.png";
+
+const X = () => {
+  return <img src={x} alt="" />;
+};
+
+export default X;
